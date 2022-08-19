9b8d8188efe272603258173ab170bdfe4bb76287
katiullahaxioned <atiullahk@axioned.com>
2022-08-19 16:02:17 +0530

    small changes in js program

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 var container = document.querySelector(".container");
 var playGame = document.querySelector(".play-game");
 var selectInsect = document.querySelector(".select-insect");
+var gameMain = document.querySelector(".game-main");
 var playButton = document.querySelector(".play-button");
 var insect = document.querySelectorAll(".insect");
 var timeCount = document.querySelector(".time-count");
 var scoreCount = document.querySelector(".score-count");
 var gameDisplay = document.querySelector(".game-display");
+var gameAlert = document.querySelector(".game-alert");
 
 playButton.addEventListener("click", function () {
-  playGame.classList.add("hide");
+  playGame.classList.add("viewport-inactive");
+  selectInsect.classList.add("viewport-active");
 });
 
 insect[0].classList.add("insect-active");
@@ -24,15 +27,22 @@ insect.forEach(function (element) {
     element.classList.add("insect-active");
 
     setTimeout(function () {
-      selectInsect.classList.add("hide");
+      selectInsect.classList.remove("viewport-active");
+      selectInsect.classList.add("viewport-inactive");
+      gameMain.classList.add("viewport-active");
     }, 300);
 
     var countNumber = 0;
+    var initial = 0;
     var insectStart = setInterval(function () {
       var a = Math.trunc(Math.random() * 100);
       var b = Math.trunc(Math.random() * 100);
       var cloneImage = image.cloneNode(true);
-      if (selectInsect.classList.contains("hide")) {
+      if (selectInsect.classList.contains("viewport-inactive")) {
+        if(a > 95 || b > 95) {
+          a -= 5;
+          b -= 5;
+        }
         cloneImage.style = `width: 5%;
 														position: absolute;
 														top: ${a}%;
@@ -44,28 +54,32 @@ insect.forEach(function (element) {
           countNumber++;
           scoreCount.innerHTML = countNumber;
         });
+        initial++;
+        if(initial === 15) {
+          clearInterval(insectStart);
+          for(var j=0; j<gameDisplay.children.length; j++) {
+            gameDisplay.children[j].classList.add('pointer-disable');
+          }
+        }
+        setTimeout(function() {
+          gameAlert.classList.add('alert-active');
+          if(initial > 10) {
+          gameAlert.classList.remove('alert-active');
+          }
+        }, 5000)
       }
-    }, 500);
+    }, 1000);
 
     var secondCount = 0;
     var timeStart = setInterval(function () {
       secondCount++;
-      if (secondCount < 10) {
+      if(secondCount > 15) {
+        clearInterval(timeStart);
+      } else if (secondCount < 10) {
         timeCount.innerHTML = `00:0${secondCount}`;
       } else {
         timeCount.innerHTML = `00:${secondCount}`;
       }
     }, 1000);
-
-    setTimeout(function () {
-      clearInterval(timeStart);
-      clearInterval(insectStart);
-      setTimeout(function () {
-        alert("Your final score is: " + countNumber);
-        for (var i = 0; i < gameDisplay.children.length; i++) {
-          gameDisplay.children[i].classList.add("hide");
-        }
-      }, 10);
-    }, 5000);
   });
 });
